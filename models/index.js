10237b7eeade885f98d0c6ac47be319f9dfa8bd4
iambryantran <139257318+iambryantran@users.noreply.github.com>
2023-11-21 14:43:38 -0600

    finished models and associations

--- models/index.js
+++ models/index.js
@@ -8,25 +8,26 @@ const ProductTag = require('./ProductTag');
 
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-})
+});
 
 // Categories have many Products
 
 Category.hasMany(Product, {
   foreignKey: 'category_id'
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  through: ProductTag
-  
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
